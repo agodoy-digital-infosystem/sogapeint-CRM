9390648a7ef9574d97c8183fdb74c753ff0eb7ec
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2023-12-08 16:54:30 +0100

    Add rate limiting to login route to prevent brute force attacks

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,22 @@
 // routes/authRoutes.js
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
 console.log('authController', authController);
 
+// Limiter pour la route de connexion
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limite chaque IP à 5 tentatives de connexion
+    message: 'Trop de tentatives de connexion depuis cette IP, veuillez réessayer après 15 minutes'
+  });
+
 // Route pour l'inscription
 // router.post('/signup', authController.signup);
 
 // Route pour la connexion
-router.post('/login', authController.login);
+router.post('/login',loginLimiter, authController.login);
 
 module.exports = router;
