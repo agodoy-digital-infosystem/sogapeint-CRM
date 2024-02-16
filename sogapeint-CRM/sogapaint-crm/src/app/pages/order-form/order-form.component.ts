ad10776002d9418f715be54a89ed888d9b82ac6e
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-02-16 14:29:58 +0100

    order-form : UI improvement

--- sogapeint-CRM/sogapaint-crm/src/app/pages/order-form/order-form.component.ts
+++ sogapeint-CRM/sogapaint-crm/src/app/pages/order-form/order-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order-form',
   templateUrl: './order-form.component.html',
   styleUrls: ['./order-form.component.scss']
 })
-export class OrderFormComponent {
+export class OrderFormComponent implements OnInit {
+  // bread crumb items
+  breadCrumbItems: Array<{}>;
 
+    constructor() { }
+  
+    ngOnInit(): void {
+      this.breadCrumbItems = [{ label: 'Sogapeint' }, { label: 'Saisir une commande', active: true }];
+    }
 }
